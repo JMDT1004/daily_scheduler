5c3562f5f9e9bf4d4e1c71a3a211919f854a351e
JT <raikiri24@gmail.com>
2023-06-15 10:15:58 -0400

    localStorage and save button

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 var today = dayjs();
 $('#currentDay').text(today.format('dddd, MMMM DD, YYYY'));
 
+//establish save button and user Input local Storage
 $(document).ready(function () {
   $('.saveBtn').on('click', function () {
     var timeBlock = $(this).closest('.time-block');
@@ -11,10 +12,12 @@ $(document).ready(function () {
     localStorage.setItem(timeBlockId, userInput);
   });
 
+  //set current hour tracker for scheduler
   var currentHour = dayjs().get('hour');
   $('.time-block').each(function () {
     var timeBlockId = parseInt($(this).attr('id'));
 
+    //added 'past''present''future' designation to match time of day
     if (timeBlockId < currentHour) {
       $(this).addClass('past');
     } else if (timeBlockId === currentHour) {
